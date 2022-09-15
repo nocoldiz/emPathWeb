b43af13675a3ad3d3bf43a30abde6e079a43e362
John Doe <johndoe@example.com>
2022-09-15 22:25:48 +0200

    feat: added some descriptions

--- src/app/advMode/adventure/adventureModules/varlenia.places.ts
+++ src/app/advMode/adventure/adventureModules/varlenia.places.ts
@@ -13,33 +13,36 @@ export let fixedPlaces: IPlace[] = [
   {
     id: 'ml-gas-station',
     name: 'Moonlit gas station',
-    description: 'Test',
+    description: `The moonlit gas station is so far from home. The sign says "Honk if you're lonely," but nobody does. There's nothing here for anyone. It doesn't have its own name, because it's too far away from anybody else to be worth naming.
+    It's the middle of nowhere, and it's always empty. Nobody comes this way, except maybe occasionally some trucker who needs a break from driving.There is a small convenience store in the gas station that sells snacks and drinks, but no fresh produce or meat. If you get hungry here, you can eat the snack foods, which are all pretty old by now. They taste like they've been there forever.
+    `,
     actions: ['lit-fire', 'enter-camper', 'lick', 'open', 'listen'],
     places: ['ml-bathroom', 'ml-gas-station-store', 'ml-parking-lot'],
   },
   {
     id: 'ml-bathroom',
-    name: 'Bathroom',
-    description: 'Test',
+    name: `Bathroom`,
+    description: `The bathroom door creaks loudly when you push it open and there are no lights on in the room. A single light bulb hangs from the ceiling, but it's broken and only gives off a dim glow. Inside the room, there are several stalls with doors that hang slightly crooked and have been nailed back together. The toilet paper dispenser looks like it was pulled out by an angry bear, and all the towels look like they've had their collars ripped off. There is a mirror on one wall, which has seen better days, but at least there's a sink for washing your hands. When you turn on the faucet to wash them, only a dribble comes out. `,
     places: ['ml-gas-station', 'ml-bathroom-stall'],
   },
   {
     id: 'ml-bathroom-stall',
     name: 'Bathroom stall',
-    description: 'Test',
+    description: `There is a sign that says "please use the bathroom" and a small hand print next to it.
+    If you look up, you can see the sky through the hole in the roof. It doesn't rain often here and most people don't mind the lack of privacy or having to go outside to do their business.`,
     actions: ['piss'],
     places: ['ml-bathroom'],
   },
   {
     id: 'ml-parking-lot',
     name: 'Parking lot',
-    description: 'Test',
+    description: `The parking lot is gravel, and there's no grass anywhere around here. There isn't even any wildlife; just a few stray dogs that sometimes wander in when someone goes into the convenience store to buy food. The only plants growing in the area are weeds; the weeds don't seem to mind being alone.' `,
     places: ['ml-gas-station'],
   },
   {
     id: 'ml-gas-station-store',
     name: 'Store',
-    description: 'Store',
+    description: `Store`,
     places: ['ml-gas-station'],
   },
   {
--- src/app/advMode/placeInfo/place-info.component.ts
+++ src/app/advMode/placeInfo/place-info.component.ts
@@ -3,6 +3,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { IPlace } from '../../interfaces/places.interface';
+import {
+  loadPlace,
+  loadPreviousPlace,
+} from 'src/app/store/scene/scene.actions';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-place-info',
@@ -11,8 +16,12 @@ import { IPlace } from '../../interfaces/places.interface';
 })
 export class PlaceInfoComponent implements OnInit {
   @Input() place: IPlace;
-
-  constructor() {}
+  @Input() reachablePlaces: IPlace[];
+  clickOnPlace(newPlace: IPlace): void {
+    console.log('## click on place ', newPlace);
+    this.store.dispatch(loadPlace({ place: newPlace }));
+  }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {}
 }
