22d8937c9284d9bc9bb5b75c53a66efd0c21b0f9
John Doe <johndoe@example.com>
2022-09-22 01:02:31 +0200

    feat: wfc map generation

--- src/app/advMode/actionsPanel/actions-panel.component.ts
+++ src/app/advMode/actionsPanel/actions-panel.component.ts
@@ -8,8 +8,11 @@ import { IItem } from 'src/app/interfaces/inventory.interface';
 import { INpc } from 'src/app/interfaces/npc.interface';
 import { Field } from './sketch/wfc/wfc.js';
 import { IPlace } from 'src/app/interfaces/places.interface';
-
-import * as p5 from 'p5';
+import {
+  IWaveFunctionCollapse,
+  createWaveFunctionCollapse,
+} from '../wfc/wfc/run';
+import { createWfcOptions } from '../wfc/components/wfcOptions';
 
 @Component({
   selector: 'app-actions-panel',
@@ -28,123 +31,44 @@ export class ActionsPanelComponent implements OnInit {
   sampleImage;
   @ViewChild('canvas', { static: true })
   canvas: ElementRef<HTMLCanvasElement>;
-  WFC;
 
-  readyToGenerate = false;
-  finished = false;
+  @ViewChild('mapContainer') mapContainer: ElementRef;
 
-  steps = 100;
-  avg_steps = 0;
-  rendered_frames = 0;
-  url_params;
-  tileH;
-  tileW;
-  tileSpacing;
-  tileBorderRadius;
-  sw;
-  widthDivider;
-  heightDivider;
-  background_color = '#0f0f25';
-  displayBackgroundTiles = true;
-  main_timer = 0;
-  total_collapse_count = 0;
-  width = 200;
-  height = 300;
+  inputBitmap: ImageData | undefined;
 
-  drawCell;
+  wfc: IWaveFunctionCollapse | undefined;
+  getImageData(url: string): Promise<ImageData> {
+    const img = document.createElement('img');
+    img.src = url;
 
-  clickOnAction(action: IAction): void {
-    console.log('## click on action ', action);
-    this.store.dispatch(
-      updateLog({ logEntry: { text: 'test', action: 'you made x' } })
-    );
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    return new Promise((resolve, reject) => {
+      img.addEventListener('load', () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        resolve(ctx.getImageData(0, 0, canvas.width, canvas.height));
+      });
+    });
   }
-  private onWindowResize = (e) => {
-    this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
-  };
 
-  private createCanvas = () => {
-    console.log('creating canvas');
-    this.p5 = new p5(this.drawing);
-  };
-  private drawing = function (p: any) {
-    // f5 setup
-    p.setup = () => {
-      p.createCanvas(300, 500).parent('gear-clock-canvas');
-      p.angleMode(p.DEGREES);
-      p.rectMode(p.CENTER);
-      p.background(0);
-    };
-    p.center = { x: 0, y: 0 };
-    // f5 draw
-    p.draw = () => {
-      p.background(0);
-      p.center.x = p.width / 2;
-      p.center.y = p.height / 2;
-
-      let hr = p.hour();
-      let mn = p.minute();
-      let sc = p.second();
-      let ms = p.millis();
-
-      p.push();
-
-      p.translate(p.center.x, p.center.y);
-      p.rotate(-90);
-
-      p.strokeWeight(8);
-      p.noFill();
-
-      // dail
-      p.stroke(175);
-      p.arc(0, 0, 210, 210, 0, 360);
-
-      // second
-      let sc_end = p.map(sc % 60, 0, 60, 0, 360);
-
-      p.push();
-      p.rotate(sc_end);
-      p.stroke(255, 0, 0);
-      p.line(0, 0, 90, 0);
-      p.pop();
-
-      // minute
-      let mn_end = p.map(mn % 60, 0, 60, 0, 360);
-      p.push();
-      p.rotate(mn_end);
-      p.stroke(0, 230, 0);
-      p.line(0, 0, 70, 0);
-      p.pop();
-
-      // hour
-      let hr_end = p.map(hr % 12, 0, 12, 0, 360);
-      p.push();
-      p.rotate(hr_end);
-      p.stroke(0, 0, 230);
-      p.line(0, 0, 50, 0);
-      p.pop();
-
-      // center
-      p.fill(255);
-      p.noStroke();
-      p.ellipse(0, 0, 8, 8);
-
-      p.pop();
-
-      let clock = hr + ':' + mn + ':' + sc;
-      p.fill(255);
-      p.noStroke();
-      p.textSize(14);
-      p.text(clock, 100, 50);
-    };
-  };
-  private destroyCanvas = () => {
-    console.log('destroying canvas');
-    this.p5.noCanvas();
-  };
   constructor(private store: Store<AppState>) {}
 
-  ngOnInit(): void {
-    this.createCanvas();
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const canvas = document.createElement('canvas');
+    const wfcOptions = createWfcOptions();
+
+    canvas.className = 'wfcOutput';
+    canvas.width = 300;
+    canvas.height = 300;
+    this.mapContainer.nativeElement.append(canvas);
+
+    this.getImageData('./assets/img/wfc/Cat.png').then((image) => {
+      this.wfc = createWaveFunctionCollapse(image, canvas, wfcOptions.options);
+    });
   }
 }
--- src/app/advMode/wfc/wfc/run.ts
+++ src/app/advMode/wfc/wfc/run.ts
@@ -1,10 +1,9 @@
-import { printPatterns } from "./debug";
-import { createSuperposition } from "./superposition";
-import { createOverlappingModel } from "./overlappingModel";
-import { setGround } from "./setGround";
-import { createObservation } from "./observe";
-import { propagate } from "./propagate";
-import { createRender } from "./render";
+import { createSuperposition } from './superposition';
+import { createOverlappingModel } from './overlappingModel';
+import { setGround } from './setGround';
+import { createObservation } from './observe';
+import { propagate } from './propagate';
+import { createRender } from './render';
 
 export interface IWfcOptions {
   periodicInput: boolean;
@@ -26,24 +25,32 @@ const targetTime = 1000 / targetFps;
 export function createWaveFunctionCollapse(
   image: ImageData,
   canvas: HTMLCanvasElement,
-  { periodicInput, periodicOutput, outputWidth, outputHeight, N, symmetry, ground }: IWfcOptions,
+  {
+    periodicInput,
+    periodicOutput,
+    outputWidth,
+    outputHeight,
+    N,
+    symmetry,
+    ground,
+  }: IWfcOptions
 ): IWaveFunctionCollapse {
-
   const model = createOverlappingModel(image, { N, symmetry, periodicInput });
-  const superpos = createSuperposition(
-    model.numCoefficients,
-    { width: outputWidth, height: outputHeight, periodic: periodicOutput },
-  );
+  const superpos = createSuperposition(model.numCoefficients, {
+    width: outputWidth,
+    height: outputHeight,
+    periodic: periodicOutput,
+  });
 
   const observe = createObservation(model, superpos);
 
   canvas.width = superpos.width;
   canvas.height = superpos.height;
 
-  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
   const clear = () => {
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     superpos.clear();
     setGround(ground, superpos);
@@ -60,7 +67,7 @@ export function createWaveFunctionCollapse(
       const result = observe();
       if (result === null) {
         propagating = true;
-      } else if (result === false)  {
+      } else if (result === false) {
         clear();
       } else {
         return;
