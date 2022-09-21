7711fa9ea0f6f40985d08a4f477d6d5d39e2c1f5
John Doe <johndoe@example.com>
2022-09-21 23:13:11 +0200

    feat: better implementation of p5

--- src/app/advMode/actionsPanel/actions-panel.component.ts
+++ src/app/advMode/actionsPanel/actions-panel.component.ts
@@ -8,14 +8,9 @@ import { IItem } from 'src/app/interfaces/inventory.interface';
 import { INpc } from 'src/app/interfaces/npc.interface';
 import { Field } from './sketch/wfc/wfc.js';
 import { IPlace } from 'src/app/interfaces/places.interface';
-import {
-  createCanvas,
-  strokeWeight,
-  background,
-  rect,
-  noStroke,
-  line,
-} from 'p5';
+
+import * as p5 from 'p5';
+
 @Component({
   selector: 'app-actions-panel',
   templateUrl: './actions-panel.component.html',
@@ -27,6 +22,7 @@ export class ActionsPanelComponent implements OnInit {
   @Input() npc: INpc[];
   @Input() place: IPlace[];
   private ctx: CanvasRenderingContext2D;
+  private p5;
 
   img: string = '';
   sampleImage;
@@ -57,154 +53,98 @@ export class ActionsPanelComponent implements OnInit {
 
   drawCell;
 
-  setup() {
-    this.canvas = createCanvas(100, 200);
-    // randomSeed(0);
-    this.displayBackgroundTiles = true;
-    //  this.createDrawCell();
-
-    const imagePath = `data/${this.url_params.pattern || 'demo-3'}.png`;
-    /*
-    this.sampleImage = loadImage(imagePath, this.createField, () => {
-      alert("Image couldn't be loaded );");
-    });*/
-  }
-  draw() {
-    if (this.readyToGenerate) {
-      let time_start = performance.now();
-
-      for (let row of this.WFC.grid) for (let elt of row) elt.display();
-
-      let i = 0;
-      if (!this.finished)
-        while (i++ < 1000) {
-          this.WFC.updateStep();
-
-          if (i % 10 == 0) if (performance.now() - time_start > 33.3) break;
-        }
-      this.avg_steps += i;
-      // WFC.updateChunk();/
-
-      this.rendered_frames++;
-
-      // if (finished) {
-      //   randomSeed(0);
-      //   createField();
-      //   finished = !finished;
-      // }
-    } else {
-      background(0, 10, 60);
-    }
-  }
-  createField = async (_) => {
-    const N = Number(this.url_params.n || '3');
-    const symmetry = Number(this.url_params.symmetry || '1');
-    Field.createFromImage(
-      this.sampleImage,
-      N,
-      symmetry,
-      Math.floor(
-        Math.floor(this.width / this.widthDivider) / this.sampleImage.width + 1
-      ) * this.sampleImage.width,
-      Math.floor(this.height / this.heightDivider)
-    ).then((field) => {
-      this.WFC = field;
-      this.WFC.seed();
-
-      this.readyToGenerate = true;
-      background(this.background_color);
-
-      this.tileW = this.width / Math.floor(this.width / this.widthDivider);
-      this.tileH = this.height / Math.floor(this.height / this.heightDivider);
-      this.tileSpacing = Math.min(this.tileH, this.tileW) / 8;
-      this.tileBorderRadius = this.tileSpacing * 1.3;
-      this.sw = (this.tileW - this.tileSpacing) / 4;
-
-      console.log('Succesfully finished loading...');
-      console.log({ width: this.WFC.W, height: this.WFC.H });
-      this.main_timer -= performance.now();
-    });
-  };
-
-  createDrawCell = (): void => {
-    if (Number(this.url_params.stitches || '0')) {
-      this.widthDivider = 28;
-      this.heightDivider = 22;
-      this.drawCell = (x, y, w, h, b = false) => {
-        x = x + this.tileSpacing / 2;
-        y = y + this.tileSpacing / 2;
-        w = w - this.tileSpacing;
-        strokeWeight(b ? this.sw + 1 : this.sw);
-        line(
-          x + this.sw / 2 - w / 32,
-          y + this.sw / 2,
-          x - this.sw / 2 + w / 2 - this.sw / 16,
-          y + h
-        );
-        line(
-          x - this.sw / 2 + w + w / 32,
-          y + this.sw / 2,
-          x + this.sw / 2 + w / 2 + this.sw / 16,
-          y + h
-        );
-      };
-    } else {
-      this.widthDivider = 24;
-      this.heightDivider = 24;
-      this.drawCell = (x, y, w, h, b = false) => {
-        noStroke();
-        b
-          ? rect(x, y, w, h, this.tileBorderRadius)
-          : rect(
-              x + this.tileSpacing / 2,
-              y + this.tileSpacing / 2,
-              w - this.tileSpacing,
-              h - this.tileSpacing,
-              this.tileBorderRadius
-            );
-      };
-    }
-  };
-
-  // public actions: IAction[] = [];
-  /*
-  public sceneUpdate$ = this.store.select(getScene);
-  public actions$ = this.store.select(getScene);
-
-  public place$ = this.store.select(getPlace).subscribe((place: IPlace) => {
-    console.log(place);
-    this.title = place.name;
-
-    this.text = place.description;
-    this.store.dispatch(
-      setScene({ title: place.name, text: place.description })
-    );
-    place?.actions.forEach((actionId) => {
-      let placeAction = actions.find((action) => action.id === actionId);
-      this.actions.push(placeAction);
-      //this.store.dispatch(pushAction({ action: placeAction }));
-
-      console.log('place?.actions', actionId, placeAction);
-    });
-  });
-  public activeAction$ = this.store.select(getActiveAction).subscribe((id) => {
-    console.log('## active action', id);
-  });
-*/
   clickOnAction(action: IAction): void {
     console.log('## click on action ', action);
     this.store.dispatch(
       updateLog({ logEntry: { text: 'test', action: 'you made x' } })
     );
   }
+  private onWindowResize = (e) => {
+    this.p5.resizeCanvas(this.p5.windowWidth, this.p5.windowHeight);
+  };
 
+  private createCanvas = () => {
+    console.log('creating canvas');
+    this.p5 = new p5(this.drawing);
+  };
+  private drawing = function (p: any) {
+    // f5 setup
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight).parent('gear-clock-canvas');
+      p.angleMode(p.DEGREES);
+      p.rectMode(p.CENTER);
+      p.background(0);
+    };
+    p.center = { x: 0, y: 0 };
+    // f5 draw
+    p.draw = () => {
+      p.background(0);
+      p.center.x = p.width / 2;
+      p.center.y = p.height / 2;
+
+      let hr = p.hour();
+      let mn = p.minute();
+      let sc = p.second();
+      let ms = p.millis();
+
+      p.push();
+
+      p.translate(p.center.x, p.center.y);
+      p.rotate(-90);
+
+      p.strokeWeight(8);
+      p.noFill();
+
+      // dail
+      p.stroke(175);
+      p.arc(0, 0, 210, 210, 0, 360);
+
+      // second
+      let sc_end = p.map(sc % 60, 0, 60, 0, 360);
+
+      p.push();
+      p.rotate(sc_end);
+      p.stroke(255, 0, 0);
+      p.line(0, 0, 90, 0);
+      p.pop();
+
+      // minute
+      let mn_end = p.map(mn % 60, 0, 60, 0, 360);
+      p.push();
+      p.rotate(mn_end);
+      p.stroke(0, 230, 0);
+      p.line(0, 0, 70, 0);
+      p.pop();
+
+      // hour
+      let hr_end = p.map(hr % 12, 0, 12, 0, 360);
+      p.push();
+      p.rotate(hr_end);
+      p.stroke(0, 0, 230);
+      p.line(0, 0, 50, 0);
+      p.pop();
+
+      // center
+      p.fill(255);
+      p.noStroke();
+      p.ellipse(0, 0, 8, 8);
+
+      p.pop();
+
+      let clock = hr + ':' + mn + ':' + sc;
+      p.fill(255);
+      p.noStroke();
+      p.textSize(14);
+      p.text(clock, 100, 50);
+    };
+  };
+  private destroyCanvas = () => {
+    console.log('destroying canvas');
+    this.p5.noCanvas();
+  };
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.ctx = this.canvas.nativeElement.getContext('2d');
-    this.ctx.fillStyle = 'red';
-    this.ctx.fillRect(0, 0, 5, 5);
-
-    this.setup();
+    this.createCanvas();
   }
 }
